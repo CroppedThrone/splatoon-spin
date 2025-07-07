2e1f1d46e485e932749ca5f40addd658149709a0
donthurtme1 <malwarecunt@proton.me>
2025-07-07 21:54:05 +0100

    idk stuff

--- SplatoonSpin/Assets/Script/CameraMovement.cs
+++ SplatoonSpin/Assets/Script/CameraMovement.cs
@@ -2,18 +2,14 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+	public PlayerInput player_input;
     public Transform target_transform;
 	public Transform camera_object;
-
-    void
-	Start()
-    {
-        
-    }
 
     void
 	Update()
     {
         camera_object.transform.LookAt(target_transform);
+		camera_object.transform.y += player_input.Look.x;
     }
 }
